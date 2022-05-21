a9d1021b8fcc6ab622c9abbdaa29c5d8d4cb8201
Holly Grimm <holly.grimm@gmail.com>
2022-05-21 15:15:54 -0600

    when minBid changes, update form validity

--- packages/dapp/src/app/features/nfts/nft/nft.component.ts
+++ packages/dapp/src/app/features/nfts/nft/nft.component.ts
@@ -126,6 +126,7 @@ export class NFTComponent implements OnInit, OnDestroy {
       // format the min bid into Ether and create a number
       const minBidEther = utils.formatEther(new BigNumber(minBid).toString());
       this.minBid = parseFloat(minBidEther);
+      this.formGroup.controls['amount'].updateValueAndValidity();
     });
 
     this.store.dispatch(NFTActions.bidsLoadByNFT({ nftId: this.id.toString() }));
